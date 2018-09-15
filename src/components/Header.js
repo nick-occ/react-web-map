8ea8c9b8c788de9924bab8d50b0ac281d0aca6cb
nick-occ <nicholaso1980@gmail.com>
2018-09-14 23:38:19 -0400

    show heading from database

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import headerConfig from '../../config/headerConfig.json';
 import HeaderItem from './HeaderItem';
 
-const Header = (props) => {
-    let menuItems = headerConfig.menuItems.sort((a, b) => a.order - b.order);
+export class Header extends Component {
 
-   return (
-       <header className='header'>
-        <h1 className='header__title'>{props.title}</h1>
-        <div className='header__items'>
-            {
-                menuItems.map((item) => <HeaderItem className='header__item' key={item.name} item={item} />)
-            }
-        </div>
-        
-
-    </header>);
+    constructor() {
+        super();
+    };
+    
+   render() {
+        let menuItems = headerConfig.menuItems.sort((a, b) => a.order - b.order);
+        return (
+            <header className='header'>
+                <h1 className='header__title'>{this.props.map.name}</h1>
+                <div className='header__items'>
+                    {
+                        menuItems.map((item) => <HeaderItem className='header__item' key={item.name} item={item} />)
+                    }
+                </div>
+            </header>
+        );
+    }
 };
 
-Header.defaultProps = {
-    title: 'Map App'
-}
+const mapStateToProps = state => {
+    return { map: state.map };
+};
 
-export default Header;
+export default connect(mapStateToProps)(Header);
