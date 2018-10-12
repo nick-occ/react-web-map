7ec81d2cfde746b56d9f07095387110fb5fc109a
nick-occ <nicholaso1980@gmail.com>
2018-10-11 23:25:45 -0400

    refactored home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,29 +1,54 @@
 import React from 'react';
 
 import EsriMap from './EsriMap';
+import EsriMap1 from './EsriMap1';
 import Header from './Header';
 import Legend from './Legend';
 import SidePanel from './SidePanel';
 
+import {getToken, getConfig} from '../actions/map';
+import { getMapName, getSearchResults } from '../selectors/map';
+import connect from "react-redux/es/connect/connect";
+
 class Home extends React.Component {
 
-    constructor(props) {
+    constructor() {
       super();
+    }
+
+    componentDidMount() {
+        this.props.getToken();
+        this.props.getConfig();
     }
   
     render() {
-      const title = 'Map';
       return (
           <div id='main-frame'>
-              <Header title={title}/>
+              <Header
+                  title={getMapName(this.props.map)}
+              />
               <div id={'map-frame'}>
-                  <EsriMap/>
-                  <SidePanel/>
+                  <EsriMap1/>
+                  {/*<EsriMap/>*/}
+                  <SidePanel
+                      searchResults={getSearchResults(this.props.map)}
+                  />
                   <Legend/>
               </div>
           </div>
       );
     }
   }
-  
-  export default Home;
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        getToken: () => dispatch(getToken()),
+        getConfig: () => dispatch(getConfig())
+    };
+};
+
+const mapStateToProps = state => {
+    return { map: state.map };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
