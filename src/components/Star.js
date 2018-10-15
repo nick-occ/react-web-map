8fefec472e978b1f20817439fdf7106652dde3bc
nick-occ <nicholaso1980@gmail.com>
2018-10-14 23:08:29 -0400

    refactored star to use images

--- src/components/Star.js
+++ src/components/Star.js
@@ -1,18 +1,28 @@
+import _ from 'lodash'
 import React from 'react';
 
+//sets the fill portion of the star
+const fillStar = (id, rating) => {
+    let starImage;
+    if (id <= rating) {
+        starImage = 'star-full.png';
+    } else if (id === Math.ceil(rating) && rating % 2 !== 0) {
+        starImage = 'star-half.png';
+    } else {
+        starImage = 'star-empty.png';
+    }
+    return `/img/star/${starImage}`
+};
+
 const Star = (props) => {
-    //draw star polygons
-    let {fillStar} = props;
-    return (
-        <div className='star'>
-            <svg height="20" width="20">
-                <polygon className={fillStar[0]} id='star-upperleft' points="0,7 7,6 10,0 10,13"/>
-                <polygon className={fillStar[1]} id='star-lowerleft' points="10,13 3,20, 7,7"/>
-                <polygon className={fillStar[2]} id='star-upperright' points="20,7 13,6 10,0 10,13"/>
-                <polygon className={fillStar[3]} id='star-lowerright' points="10,13 17,20, 13,7"/>
-            </svg>
-        </div>
-    )
+    return _.range(1, props.totalStars + 1).map((i) => {
+        //draw star polygons
+        return (
+            <div key={i} className='star'>
+                <img src={fillStar(i, props.rating)}/>
+            </div>
+        )
+    });
 };
 
 export default Star;
